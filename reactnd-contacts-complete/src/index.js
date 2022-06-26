32c815ed6edffac52ec1fff2cd684d3bad3bdf6b
omar khattab <25khattab@gmail.com>
2022-06-27 01:02:34 +0200

    init

--- reactnd-contacts-complete/src/index.js
+++ reactnd-contacts-complete/src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-
+console.log("you deployed");
 ReactDOM.render(
   <BrowserRouter><App /></BrowserRouter>,
   document.getElementById('root')
